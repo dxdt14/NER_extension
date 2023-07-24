f0918d725a5ca2a1035af878639e7a2b70c89d69
Vidhata Jayaraman <vidhata2004@gmail.com>
2023-07-23 21:23:42 -0400

    Changed ReadMe

--- WikiExtension/hightext.js
+++
@@ -1,9 +0,0 @@
-import './dist/jquery.mark.js';
-export function highlightKeyWords(arr){
-    $(document.querySelectorAll("body")).mark(arr, {
-        "element": "span",
-        "className": "highlight"
-    });
-    // var instance = new Mark(document.querySelectorAll("body"));
-    // instance.mark(arr, {"element": "span", "className": "highlight"});
-}
